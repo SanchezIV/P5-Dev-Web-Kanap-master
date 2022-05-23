1b075d7972a5f1c7294f5cdf0dffc26754fa466d
Emile Le Corre <emile.21@hotmail.fr>
2022-05-23 15:39:12 +0200

    ajout de la page script.js terminée

---
+++ js/script.js
@@ -0,0 +1,20 @@
+ fetch("http://localhost:3000/api/products") // se lier à l'api contenant les produits
+  .then((httpBodyResponse) => {
+    return httpBodyResponse.json(); //on transforme la reponse au format json
+  })
+  .then((products) => {
+    for (let product of products) { //on déclare la variable "product" de la liste de "products"
+      console.log(product)
+      document.querySelector('#items').innerHTML += `<a href="./product.html?id=${product._id}"> 
+    <article>
+      <img src="${product.imageUrl}" alt="${product.altTxt}">
+      <h3 class="productName">${product.name}</h3>
+      <p class="productDescription">${product.description}</p>
+    </article>
+  </a>` // on sélectionne la class html a modifier et on remplis avec les infos des "product"
+    }
+  })
+  .catch((err) => {
+    alert('error' + err); // en cas d'erreur le site renvoi un message d'alerte
+  });
+  
