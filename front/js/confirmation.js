59f05f4718912a9e959d231ba87065d1c1e60428
Emile Le Corre <emile.21@hotmail.fr>
2022-05-25 15:44:42 +0200

    finalisation de la confirmation.js

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,3 +1,6 @@
 var str = window.location.href;
 var url = new URL(str);
 var orderId = url.searchParams.get("order");
+console.log(orderId);
+let confirm = document.getElementById("orderId");
+confirm.textContent = orderId;
